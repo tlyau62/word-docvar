9472c51eb858e79ee4af876747a3c9ada6252b3c
Tim Yau <tlyau62@gmail.com>
2022-11-04 17:00:20 +0800

    First

---
+++ WordDocVar/HtmlControlReplacer.cs
@@ -0,0 +1,65 @@
+﻿using DocumentFormat.OpenXml;
+using OpenXMLTemplates;
+using OpenXMLTemplates.ControlReplacers;
+using OpenXMLTemplates.Documents;
+using OpenXMLTemplates.Variables;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Xml.Linq;
+using System.IO;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace WordDocVar
+{
+    public class HtmlControlReplacer : ControlReplacer
+    {
+        public override string TagName => "html";
+
+        protected override OpenXmlExtensions.ContentControlType ContentControlTypeRestriction => OpenXmlExtensions.ContentControlType.RichText;
+
+        protected override string ProcessControl(string variableIdentifier, IVariableSource variableSource, ContentControl contentControl, List<string> otherParameters)
+        {
+            return variableSource.GetVariable<string>(variableIdentifier);
+        }
+
+        protected override void OnReplaced(ContentControl e)
+        {
+            var html = e.SdtElement.InnerText;
+            var oml = ConvertHtmlToÓml(html) as Document;
+            var nodes = oml.Body.Elements()
+                .SkipLast(1)
+                .Select(n => n.CloneNode(true));
+
+            e.SdtElement.RemoveAllChildren();
+
+            foreach (var node in nodes) { 
+                e.SdtElement.AppendChild(node);
+            }
+
+            base.OnReplaced(e);
+        }
+
+        private OpenXmlElement ToOpenXmlElement(XElement element)
+        {
+            // Write XElement to MemoryStream.
+            using var stream = new MemoryStream();
+            element.Save(stream);
+            stream.Seek(0, SeekOrigin.Begin);
+
+            // Read OpenXmlElement from MemoryStream.
+            using OpenXmlReader reader = OpenXmlReader.Create(stream);
+            reader.Read();
+            return reader.LoadCurrentElement();
+        }
+
+        private OpenXmlElement ConvertHtmlToÓml(string html)
+        {
+            var xe = XElement.Parse($"<html><body>{html}</body></html>");
+            var wml = OpenXmlPowerTools.HtmlToWmlConverter.ConvertHtmlToWml("", "", "", xe, OpenXmlPowerTools.HtmlToWmlConverter.GetDefaultSettings());
+
+            return ToOpenXmlElement(wml.MainDocumentPart);
+        }
+    }
+}
---
+++ WordDocVar/Program.cs
@@ -0,0 +1,37 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Packaging;
+using OpenXmlPowerTools;
+using OpenXMLTemplates.Documents;
+using OpenXMLTemplates.Engine;
+using OpenXMLTemplates.Variables;
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace WordDocVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new DefaultOpenXmlTemplateEngine();
+            var input = @"C:\Users\tyautl\Git\WordDocVar\WordDocVar\Resources\c.docx";
+            var output = @"C:\Users\tyautl\Git\WordDocVar\WordDocVar\Resources\c2.docx";
+
+            engine.RegisterReplacer(new HtmlControlReplacer());
+
+            using (var doc = new TemplateDocument(input))
+            {
+                var src = new VariableSource(@"{ ""companyAddr"": ""4321"", ""content"": ""<p>test</p><p>testte<b>sascacs</b>t</p><ul><li>object</li><li>object2</li></ul>""}");
+
+                engine.ReplaceAll(doc, src);
+
+                doc.SaveAs(output);
+            }
+
+            Console.WriteLine("end");
+        }
+    }
+}
